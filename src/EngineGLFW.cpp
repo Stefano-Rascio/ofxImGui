0243fd4a23b732c8775b81ccb9dec2e8509fec2e
Stefano Rascio <stefano@igloovision.com>
2019-01-16 14:32:03 +0000

    Use ImGui clipboard function, not the OF ones

--- src/EngineGLFW.cpp
+++ src/EngineGLFW.cpp
@@ -51,8 +51,9 @@ namespace ofxImGui
 			}
 		}
 
-		io.SetClipboardTextFn = &BaseEngine::setClipboardString;
-		io.GetClipboardTextFn = &BaseEngine::getClipboardString;
+		// Leave ImGui clipboard functions in place - they work!
+		//io.SetClipboardTextFn = &BaseEngine::setClipboardString;
+		//io.GetClipboardTextFn = &BaseEngine::getClipboardString;
 
 		createDeviceObjects();
 
